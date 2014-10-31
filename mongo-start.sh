8f2635fdd63cc4732ae999a88ae225f915a2cf89
Manuel Kanah <testinaweb@testinaweb.com>
2014-10-31 17:34:34 +0000

    second commit

--- mongo-start.sh
+++ mongo-start.sh
@@ -12,15 +12,31 @@ if [ "${shardAmounts}" == "" ]; then
     shardAmounts=3
 fi
 
-echo "${shardAmounts}"
+read -p "How many config servers you need? [1] : " configsvrAmounts
+if [ "${configsvrAmounts}" == "" ]; then
+    configsvrAmounts=1
+fi
+
+
+echo "Start creation..."
 
 mkdir logs
 
-for (( c=1; c<=shardAmounts; c++ ))
+# shardsvr
+for (( c=1; c<=$shardAmounts; c++ ))
 do
-    echo "... creatinng Shard ${c}"
+    echo "... creatinng shard ${c}"
     mkdir shard$c
     mongod --dbpath shard$c --port 3000$c --fork --smallfiles --logpath logs/shard$c.log --logappend --oplogSize 10 --shardsvr #--replSet <setname>
 done
 
-#mongod --configsvr --dbpath /data/configdb --port 27019
+# configsvr
+for (( c=1; c<=$configsvrAmounts; c++ ))
+do
+    echo "... creatinng config server ${c}"
+    mkdir configsvr$c
+    mongod --dbpath configsvr$c --port 2702$c --fork --smallfiles --logpath logs/configsvr$c.log --logappend --oplogSize 10 --configsvr
+done
+
+mongos --configdb localhost:27021
+#sh.addShard("rs1/mongodb0.example.net:27017")
